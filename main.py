f0ee0ef7fb7498ebfb8ed324b2be53c32a735ddf
gradan-hash <mutukunyaa@gmail.com>
2023-08-21 14:41:14 +0300

    ...

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from src.textSummarizer.logging import logger
 
-logger.info("welcoe to our custom log")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage{STAGE_NAME} started")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed \n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
